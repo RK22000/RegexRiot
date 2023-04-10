48a38136500a3976d8f2d9ba0eb45e6a039254c8
Rahul Kandekar <rahukand@gmail.com>
2023-04-10 01:02:57 -0700

    Forgot to add these files to the previous commit

---
+++ src/main/java/io/github/rk22000/RegexRiot/RiotSet.java
@@ -0,0 +1,14 @@
+package io.github.rk22000.RegexRiot;
+
+import io.github.rk22000.RegexRiot.RiotTypedSet.InclusiveSet;
+
+import static io.github.rk22000.RegexRiot.Riot.riot;
+import static io.github.rk22000.RegexRiot.RiotTypedSet.InclusiveSet.riotInclude;
+
+public interface RiotSet {
+    default RiotString toRiotString() {
+        return riot(toString(), true);
+    }
+    String rawString();
+    RiotSet complement();
+}
